bebdaac3d3b85520b2f3240626f68531a1d58261
yersie <yersih@outlook.com>
2019-01-20 04:07:29 +0500

    Now uploads in a separate thread.

--- src/yersih/bayaanulquran/db/DropToBox.java
+++ src/yersih/bayaanulquran/db/DropToBox.java
@@ -3,6 +3,9 @@ package yersih.bayaanulquran.db;
 import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -24,14 +27,34 @@ public class DropToBox {
         clientV2 = new DbxClientV2(config, ACCESS_TOKEN);
     }
 
-    public void uploadDatabase() throws IOException, DbxException {
-        try (InputStream in = new FileInputStream("data/database.db")) {
-            clientV2.files().uploadBuilder(
-                    "/data/" + System.getenv().get("USERNAME") +
-                    "/" + LocalDate.now().format(DateTimeFormatter.ofPattern("Y_M_D")) +
-                    "/" + LocalDateTime.now().format(dateTimeFormatter) + "/database.db"
-            ).uploadAndFinish(in);
-        }
+    public void uploadDatabase(Button button) {
+
+        button.setDisable(true);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+        Runnable upload = () -> {
+            alert.setTitle("Save Database Alert");
+            try (InputStream in = new FileInputStream("data/database.db")) {
+
+                String path = "/data/" + System.getenv().get("USERNAME") +
+                        "/" + LocalDate.now().format(DateTimeFormatter.ofPattern("Y_M_D")) +
+                        "/" + LocalDateTime.now().format(dateTimeFormatter) + "/database.db";
+
+                clientV2.files().uploadBuilder(path).uploadAndFinish(in);
+                alert.setContentText("Database successfully saved to DropBox.");
+            } catch (IOException | DbxException e) {
+                alert.setAlertType(Alert.AlertType.ERROR);
+                alert.setContentText("Could not saved database to DropBox.\n" +
+                        "Please check your internet connection and try again.");
+            }
+
+            Platform.runLater(() -> {
+                alert.show();
+                button.setDisable(false);
+            });
+        };
+
+        new Thread(upload).start();
     }
 
 
